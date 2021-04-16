2a319ee149510b4f389eb5d0262abb819898ba66
Par <Par@123>
2021-04-16 18:34:21 +0530

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SalesComponent } from './sales/sales.component';
+import { SalesaddeditComponent } from './sales/salesaddedit/salesaddedit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalesComponent,
+    SalesaddeditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sales/salesaddedit/salesaddedit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-salesaddedit',
+  templateUrl: './salesaddedit.component.html',
+  styleUrls: ['./salesaddedit.component.scss']
+})
+export class SalesaddeditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
